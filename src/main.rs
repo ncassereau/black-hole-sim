b78e3dabb4d295a234678cf2746cc32df6441fa3
Nathan Cassereau <nathan@cassereau.fr>
2025-08-29 16:00:31 +0000

    first test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Mon carré")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        draw_rectangle(100.0, 100.0, 50.0, 50.0, RED);
+        next_frame().await;
+    }
 }
